79aa0f331abd4889eefcf04399a11a0a7c61ddf0
FerKosNik <ferafontov83@mail.ru>
2021-11-17 19:53:04 +0300

    Create Dm_Dz_7.cpp

---
+++ Dm_Dz_7.cpp
@@ -0,0 +1,13 @@
+﻿
+class IRepository {
+	virtual void Open() = 0; // бинарная десериализация в файл
+	virtual void Save() = 0; // бинарная сериализация в файл
+};
+
+
+class IMethods {
+	virtual double GetAverageScore(const FullName& name) = 0;
+	virtual string GetAllInfo(const FullName& name) = 0;
+	virtual string GetAllInfo() = 0;
+};
+
